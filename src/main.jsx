35f9cd7ea42d0b6413bcf10cd513c8f7edacd143
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-16 13:46:23 +0200

    up

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const App = () => (
-    <div>
-        <h1>Hello</h1>
-    </div>
-)
+const App = () => {
+  return React.createElement(
+    'div',
+    { className: 'title' },
+    React.createElement('h1', { className: 'title' }, 'sss')
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App/>);
+root.render(<App />);
