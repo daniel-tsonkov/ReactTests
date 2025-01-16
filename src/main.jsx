f1f11514e61e871ec7628a51af44d80e4511a4eb
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-16 11:31:12 +0200

    up

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
-import React from "react";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => (
+    <div>
+        <h1>Hello</h1>
+    </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
