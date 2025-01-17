84b7fab0ac1c5eb25c28e0a278577a99643d1d7c
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-17 11:47:04 +0200

    up

--- src/components/header/nav.jsx
+++ src/components/header/nav.jsx
@@ -1,13 +1,6 @@
-export default function Nav() {
-  const navItems = [
-    { id: '1', link: '/articles', title: 'Articles' },
-    { id: '2', link: '/sports', title: 'sports' },
-    { id: '3', link: '/Music', title: 'Music' },
-    { id: '4', link: '/Art', title: 'Art' },
-    { id: '5', link: '/Hystory', title: 'Hystory' },
-    { id: '6', link: '/Contacts', title: 'Contacts' },
-  ];
+import { navItems } from '../../utils/data';
 
+export default function Nav() {
   return (
     <nav className="mainNav">
       <ul>
---
+++ src/utils/data.js
@@ -0,0 +1,8 @@
+export const navItems = [
+    { id: '1', link: '/articles', title: 'Articles' },
+    { id: '2', link: '/sports', title: 'sports' },
+    { id: '3', link: '/Music', title: 'Music' },
+    { id: '4', link: '/Art', title: 'Art' },
+    { id: '5', link: '/Hystory', title: 'Hystory' },
+    { id: '6', link: '/Contacts', title: 'Contacts' },
+];
