84b8bf0f544d8b84182c33fa916dbc17e0842f26
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-17 11:09:31 +0200

    up

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import Header from './components/header';
 import NewsList from './components/news_list';
 
-import './styles/syles.css';
-
 const App = () => {
   return (
     <>
       <Header />
-      <NewsList />
+      {/* <NewsList /> */}
     </>
   );
 };
 
-ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
