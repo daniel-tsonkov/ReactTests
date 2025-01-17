b568c16bec93d2a1b3ce30c9752560a2a1f6a6b1
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-17 15:14:20 +0200

    up

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './components/header/index';
-import NewsList from './components/news_list';
+// import NewsList from './components/news_list';
+// import StatePlayground from './components/playgrounds/state';
 
 const App = () => {
   return (
     <>
       <Header />
+      <div className="container">{/* <StatePlayground /> */}</div>
       {/* <NewsList /> */}
     </>
   );
