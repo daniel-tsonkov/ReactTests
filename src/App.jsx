6ccc0dc5875a5860c3b14d60e2b5dca293763ec5
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-19 19:41:50 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,22 @@
 //       <h3>Count: 3</h3>
 //     </>
 //   );
-// }
+
+import { useState } from 'react';
+
 const App = () => {
+  let [count, setCount] = useState(0);
+
+  const addOne = () => setCount(count + 1);
+  const restOne = () => setCount(count - 1);
+  const setZero = () => setCount(0);
+
   return (
     <>
-      <h3>Count: 3</h3>
+      <h3>Count: {count}</h3>
+      <button onClick={addOne}>+1</button>
+      <button onClick={restOne}>-1</button>
+      <button onClick={setZero}>Reset</button>
     </>
   );
 };
