faa7d6b1935a27438c3604db009a2132bd9799f5
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-19 18:57:09 +0200

    New project React Hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,10 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import Header from './components/header/index';
-import NewsList from './components/news_list';
-// import StatePlayground from './components/playgrounds/state';
-import { newsData } from './utils/data';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-const App = () => {
-  let [news, setNews] = useState(newsData);
-
-  const getKeywords = (event) => {
-    let keywords = event.target.value;
-    let filtered = newsData.filter((item) => {
-      return item.title.indexOf(keywords) > -1;
-    });
-
-    setNews(filtered);
-  };
-
-  return (
-    <>
-      <Header getKeywords={getKeywords} />
-      <div className="container">
-        {/* <StatePlayground /> */}{' '}
-        <NewsList news={news}>
-          <br />
-          <h1>Crildreen</h1>
-        </NewsList>
-      </div>
-    </>
-  );
-};
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
