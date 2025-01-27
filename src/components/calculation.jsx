157833c87f79a895f498cd879e8428477f4a8b54
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-27 13:42:34 +0200

    up

--- src/components/calculation.jsx
+++ src/components/calculation.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react';
 
 const Calculation = () => {
+  const [calc, setCalc] = useState(0);
   const [blocks, addBlocks] = useState([1, 52, 14]);
+
+  const addBlockHandler = () => {
+    addBlocks((prevState) => [...prevState, Math.floor(Math.random() * 100)]);
+  };
+
+  const calculate = () => {
+    let random = Math.floor(Math.random() * 100);
+    setCalc(random);
+  };
+
   return (
     <>
       <h3>Blocks</h3>
       {blocks.map((block, index) => (
         <div key={index}>{block}</div>
       ))}
+      <button onClick={addBlockHandler}>Add block</button>
+      <hr />
+      calc:{calc}
+      <button onClick={calculate}>+</button>
     </>
   );
 };
