c43904f3727b606c7ccadc000992c7844be330ad
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-27 16:47:17 +0200

    up

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,9 +4,22 @@ const MyContext = createContext();
 
 const MyProvider = (props) => {
   const [stage, setStage] = useState(1);
+  const [players, setPlayers] = useState([]);
+  const [result, setResult] = useState('');
+
   return (
     <>
-      <MyContext.Provider>{props.children}</MyContext.Provider>
+      <MyContext.Provider
+        value={{
+          stage: stage,
+          players: players,
+          result: result,
+        }}
+      >
+        {props.children}
+      </MyContext.Provider>
     </>
   );
 };
+
+export { MyContext, MyProvider };
