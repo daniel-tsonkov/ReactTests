3d2fc85fbd0e0e476de3f9fcfe09e0297a006d95
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-27 11:42:13 +0200

    up

--- src/components/user_item.jsx
+++ src/components/user_item.jsx
@@ -1,4 +1,10 @@
-const UserItem = (props) => {
+import { useContext } from 'react';
+import { MyContext } from '../context';
+
+const UserItem = () => {
+  const context = useContext(MyContext);
+  console.log(context);
+
   return <>User item</>;
 };
 
