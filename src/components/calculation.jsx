c44f083b2703b2f6d42bd9ab57d86e59c84961f8
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-27 13:55:31 +0200

    up

--- src/components/calculation.jsx
+++ src/components/calculation.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 const Calculation = () => {
   const [calc, setCalc] = useState(0);
   const [blocks, addBlocks] = useState([1, 52, 14]);
+  const results = multiplay(calc);
 
   const addBlockHandler = () => {
     addBlocks((prevState) => [...prevState, Math.floor(Math.random() * 100)]);
@@ -26,5 +27,7 @@ const Calculation = () => {
     </>
   );
 };
+
+const multiplay = (calc) => {};
 
 export default Calculation;
