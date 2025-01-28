f92a51b96adabb39e6649689f87ec548063f72a9
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-28 10:08:04 +0200

    28012025

--- src/components/stage_one.jsx
+++ src/components/stage_one.jsx
@@ -1,5 +1,34 @@
+import { useRef } from 'react';
+import { Button, Form, Alert } from 'react-bootstrap';
+
 const Stage1 = () => {
-  return <>Stage 1</>;
+  const textInput = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = textInput.current.value;
+
+    console.log(value);
+    textInput.current.value = '';
+  };
+
+  return (
+    <>
+      <Form onSubmit={handleSubmit} className="mt-4">
+        <Form.Group>
+          <Form.Control
+            type="text"
+            placeholder="Add player name"
+            name="player"
+            ref={textInput}
+          />
+        </Form.Group>
+        <Button className="miami" variant="primary" type="submit">
+          Add player
+        </Button>
+      </Form>
+    </>
+  );
 };
 
 export default Stage1;
