94b37583b9ffae87272673f1c0ab6f40b95f947a
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-28 15:35:13 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,35 @@
-import { useContext, useRef } from 'react';
-import { MyContext } from './context';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style/app.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import Stage1 from './components/stage_one';
-import Stage2 from './components/stage_two';
-
-const App = () => {
-  const context = useContext(MyContext);
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="wrapper">
-      <div className="center-wrapper">
-        <h1>Koi shte plati</h1>
-        {context.stage === 1 ? <Stage1 /> : <Stage2 />}
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
-  );
-};
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
