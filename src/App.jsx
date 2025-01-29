972d76be968dd760ceba4278c08bf012a01c20cf
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-29 14:53:51 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -7,29 +7,44 @@ import Profile from './components/profile';
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">HOME</Link>
-        <Link to="posts">POST</Link>
-        <Link
-          to={{
-            pathname: 'profile',
-            hash: '#dani',
-            rearch: '?profile=true',
-          }}
-        >
-          PROFILE
-        </Link>
-      </header>
-      <hr />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="posts" element={<Post />} />
-        <Route path="profile" element={<Profile />} />
-      </Routes>
-      <footer>
+      <div className="container">
+        <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+          <Link
+            to="/"
+            className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
+          >
+            <span className="fs-4">MyAPPLogo</span>
+          </Link>
+
+          <ul className="nav nav-pills">
+            <li className="nav-item">
+              <Link to="/" className="nav-link">
+                HOME
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="posts" className="nav-link">
+                POST
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="profile" className="nav-link">
+                PROFILE
+              </Link>
+            </li>
+          </ul>
+        </header>
         <hr />
-        Footer
-      </footer>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="posts" element={<Post />} />
+          <Route path="profile" element={<Profile />} />
+        </Routes>
+        <footer>
+          <hr />
+          Footer
+        </footer>
+      </div>
     </BrowserRouter>
   );
 };
