67c4c0553e1d4089616103eb46d80849a384b8e7
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-30 09:15:17 +0200

    30012025

--- src/components/postitem.jsx
+++ src/components/postitem.jsx
@@ -1,7 +1,13 @@
+import { useParams } from 'react-router-dom'; //използва се за динамично подаване на параметри
+
 const PostItem = () => {
+  let params = useParams();
+
+  console.log(params);
+
   return (
     <>
-      <div>Post id = xxx</div>
+      <div>Post id = {params.id}</div>
     </>
   );
 };
