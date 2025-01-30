2a8e96c61dd588f548a30ff85fe10aa74c4009cf
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-30 19:35:53 +0200

    up

--- src/components/Transition.jsx
+++ src/components/Transition.jsx
@@ -2,7 +2,22 @@ import { useState } from 'react';
 
 const TransitionComp = () => {
   let [show, setShow] = useState(true);
-  return <>{show ? <div style={{ background: 'red' }}></div> : null}</>;
+
+  const showDiv = () => {
+    setShow(!show);
+  };
+
+  return (
+    <>
+      {show ? (
+        <div style={{ background: 'red', height: '100px' }}>Hello</div>
+      ) : null}
+      <hr />
+      <button className="btn btn-primary" onClick={showDiv}>
+        SHOW
+      </button>
+    </>
+  );
 };
 
 export default TransitionComp;
