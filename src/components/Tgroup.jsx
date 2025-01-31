422b3f2bbf8b6f55ec4378672d83f764fd7aa785
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-01-31 20:30:05 +0200

    up

--- src/components/Tgroup.jsx
+++ src/components/Tgroup.jsx
@@ -1,41 +1,41 @@
 import { useState } from 'react';
-
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Tgroup = () => {
-    let [items,setItems] = useState(['24','35','140']);
-    
-    const addElements = () => {
-        return items.map((item,i)=>(
-            <div className="item" key={i}>{item}</div>
-        ))
-    }
+  let [items, setItems] = useState(['24', '35', '140']);
 
-    const generateNumber = () => {
-        let newArray = [
-            ...items,
-            Math.floor(Math.random()*100)+1
-        ];
-        setItems(newArray)
-    }
+  const addElements = () => {
+    return items.map((item, i) => (
+      <CSSTransition>
+        <div className="item" key={i}>
+          {item}
+        </div>
+      </CSSTransition>
+    ));
+  };
 
-    const removeNumber= () => {
-        let newArray = items.slice(0,-1);
-        setItems(newArray)
-    }
+  const generateNumber = () => {
+    let newArray = [...items, Math.floor(Math.random() * 100) + 1];
+    setItems(newArray);
+  };
 
+  const removeNumber = () => {
+    let newArray = items.slice(0, -1);
+    setItems(newArray);
+  };
 
-    return(
-            <div className="tgroup">
-                {addElements()}
-             
-                <div className="btns">
-                    <button onClick={()=> generateNumber()}>Add Elements</button>
-                    <button onClick={()=> removeNumber()}>Remove Elements</button>
-                </div>
-            </div>
-    )
-    
-}
+  return (
+    <div className="tgroup">
+      <TransitionGroup component="div" className="list">
+        {addElements()}
+      </TransitionGroup>
 
+      <div className="btns">
+        <button onClick={() => generateNumber()}>Add Elements</button>
+        <button onClick={() => removeNumber()}>Remove Elements</button>
+      </div>
+    </div>
+  );
+};
 
 export default Tgroup;
