510c3a44239448b4467b13c8749ab0577f1af01c
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-01 14:17:55 +0200

    up

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { moviesSlice } from "./movies";
 
 export const store = configureStore({
-    reducer: {}
+    reducer: {
+        movies: moviesSlice
+    }
 });
