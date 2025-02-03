a110a16350abf8ad1756f8097e3f2d2c7cf123cf
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-03 09:46:08 +0200

    New Chapter Forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,23 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { addMovie } from './store/movies';
-import { setType, fetchUser } from './store/users';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import FormOne from './components/formOne';
+import FormThree from './components/formThree';
+import FormTwo from './components/formTwo';
+
+import Header from './components/header';
 
 const App = () => {
-  const movies = useSelector((state) => state.movies.list);
-  const users = useSelector((state) => state.users);
-  const dispatch = useDispatch();
-
-  return (
-    <>
-      <h2>Movie:</h2>
-      <ul>
-        {movies
-          ? movies.map((movie) => <li key={movie.id}>{movie.title}</li>)
-          : null}
-      </ul>
-      <hr />
-      <button onClick={() => dispatch(addMovie({ id: 3, title: 'movie31' }))}>
-        ADD MOVIE
-      </button>
-      <hr />
-      <div>{users.loading ? 'loading' : null}</div>
-      <ul>
-        {users
-          ? users.users.map((user) => {
-              <li key={user.id}>{user.name}</li>;
-            })
-          : null}
-      </ul>
-      <button onClick={() => dispatch(fetchUser({ id: '1' }))}>
-        GET USERS
-      </button>
-    </>
-  );
-};
-
-export default App;
-//Video No65 5:51
+    return(
+      <BrowserRouter>
+        <Header/>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<FormOne/>}></Route>
+            <Route path="/formtwo" element={<FormTwo/>}></Route>
+            <Route path="/formThree" element={<FormThree/>}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+    )
+  }
+  
+  export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-import { Provider } from 'react-redux';
-import { store } from './store/index.js';
-
-createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </Provider>
-);
+  </React.StrictMode>,
+)
