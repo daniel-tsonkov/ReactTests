4e95a2cf162b28a2f718acf502d2bb1eba25f51e
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-04 19:36:35 +0200

    04022025

--- src/components/formTwo.jsx
+++ src/components/formTwo.jsx
@@ -1,24 +1,27 @@
+import { useFormik } from 'formik';
+
 const FormTwo = () => {
-    return(
-        <div className="container">
-            <div className="col-md-12 mt-5">
-                <form>
-                    <label htmlFor="firstname">First name</label>
-                    <input 
-                        className="form-control" 
-                        type="text" 
-                        name="firstname"
-                    />
-                    
-                    <hr className="mb-4" />
-                    <button className="btn btn-primary btn-lg btn-block" type="submit">
-                        Submit
-                    </button>
-                </form>
-            </div>
-        </div>
-        
-    )
-}
+  const formik = useFormik({
+    initialValues: { firstname: '' },
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
+  return (
+    <div className="container">
+      <div className="col-md-12 mt-5">
+        <form>
+          <label htmlFor="firstname">First name</label>
+          <input className="form-control" type="text" name="firstname" />
+
+          <hr className="mb-4" />
+          <button className="btn btn-primary btn-lg btn-block" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default FormTwo;
