4ad19291e5b383e1387b4f7772203e7b44943df5
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-05 11:50:14 +0200

    уп

--- src/components/formThree.jsx
+++ src/components/formThree.jsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import { Formik, Form } from 'formik';
 
 const FormThree = () => {
+  const formikProps = {
+    initialValues: { firstname: '' },
+    validationSchema: Yup.object({
+      firstname: Yup.string().required('This is required'),
+    }),
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  };
+
   return (
     <div className="container">
       <div className="col-md-12 mt-5">
-        <form>
-          <label htmlFor="firstname">First name</label>
-          <input className="form-control" type="text" name="firstname" />
+        <Formik>
+          <form>
+            <label htmlFor="firstname">First name</label>
+            <input className="form-control" type="text" name="firstname" />
 
-          <hr className="mb-4" />
-          <button className="btn btn-primary btn-lg btn-block" type="submit">
-            Submit
-          </button>
-        </form>
+            <hr className="mb-4" />
+            <button className="btn btn-primary btn-lg btn-block" type="submit">
+              Submit
+            </button>
+          </form>
+        </Formik>
       </div>
     </div>
   );
