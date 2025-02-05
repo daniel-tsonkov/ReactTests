24c1fee3b42962914ce5c9755a055af5b9d87f1f
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-05 12:01:07 +0200

    up

--- src/components/formThree.jsx
+++ src/components/formThree.jsx
@@ -1,4 +1,5 @@
-import { Formik, Form } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const FormThree = () => {
   const formikProps = {
@@ -14,16 +15,17 @@ const FormThree = () => {
   return (
     <div className="container">
       <div className="col-md-12 mt-5">
-        <Formik>
-          <form>
+        <Formik {...formikProps}>
+          <Form>
             <label htmlFor="firstname">First name</label>
-            <input className="form-control" type="text" name="firstname" />
+            <Field name={firstname} type="text" className="form-control" />
+            <ErrorMessage name="firstname" />
 
             <hr className="mb-4" />
             <button className="btn btn-primary btn-lg btn-block" type="submit">
               Submit
             </button>
-          </form>
+          </Form>
         </Formik>
       </div>
     </div>
