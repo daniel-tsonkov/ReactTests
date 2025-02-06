c4afe0d108111e29bb0e8176a868c034f461a9e5
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-06 11:34:34 +0200

    up

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,6 +9,14 @@ const Header = () => {
           <Navbar.Brand>The daily news</Navbar.Brand>
         </Nav.Link>
       </Navbar>
+      <Nav>
+        <Nav.Item>
+          <Nav.Link to="/">HOME</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link to="/contact">CONTACT</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </div>
   );
 };
