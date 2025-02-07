49e6d3cd3262391173e6049aa14185e39670f51d
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-07 11:19:28 +0200

    up

--- src/components/home/homePosts.jsx
+++ src/components/home/homePosts.jsx
@@ -1,7 +1,14 @@
 import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from '../../store/utils/thunks';
 
 const HomePosts = () => {
-  useEffect(() => {}, []);
+  const homePosts = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPosts({ page: 1, order: 'desc', limit: 6 }));
+  }, []);
   return <>HOME POSTS</>;
 };
 
--- src/store/utils/thunks.js
+++ src/store/utils/thunks.js
@@ -3,10 +3,13 @@ import axios from "axios";
 const URL_SERV = "http://localhost:3001"
 
 
-export const fetchPost = createAsyncThunk(
+export const fetchPosts = createAsyncThunk(
     'posts/fetchPosts',
-    async () => {
+    async ({ page = 1, order = 'asc', limit = 10 }) => {
         try {
+            //http://localhost:3001/posts?_page=1&_limit=6&_order=desc&_sort=id
+            const response = await axios.get(`${URL_SERV}/posts?_page=${page}&_limit=${limit}&_order=${order}&_sort=id`);
+            console.log(response);
 
         } catch (err) {
             throw err
