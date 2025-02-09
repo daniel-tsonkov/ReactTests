c45474526a96a885a73d2f0df75dad5c70a233ce
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-09 19:57:57 +0200

    up

--- src/store/utils/thunks.js
+++ src/store/utils/thunks.js
@@ -23,3 +23,14 @@ export const fetchPosts = createAsyncThunk(
         }
     }
 )
+
+export const fetchPostById = createAsyncThunk(
+    'posts/fetchPostById',
+    async (id) => {
+        try {
+            const response = await axios.get(`${URL_SERV}/posts/${id}`);
+        } catch (err) {
+            throw err
+        }
+    }
+);
