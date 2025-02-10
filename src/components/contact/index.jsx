23b619f4c9d7fc87632d71b89b5060de5eac9585
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-10 10:29:42 +0200

    up

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,8 +1,24 @@
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 const Contact = () => {
+  const formik = useFormik({
+    initialValue: { email: '', firstname: '', lastname: '', message: '' },
+    validationSchema: Yup.object({
+      email: Yup.string().required('Enter email').email('invalid email'),
+      firstname: Yup.string().required('Enter text'),
+      lastname: Yup.string().required('Enter text'),
+      message: Yup.string().required('Enter text').max(500, 'Too long'),
+    }),
+    onSubmit: (valuse, { resetForm }) => {
+      console.log(valuse);
+    },
+  });
+
   return (
     <>
       <h1>Contact us</h1>
-      <form className="mt-3">
+      <form className="mt-3" onSubmit={formik.handleSubmit}>
         <div className="form-group">
           <label htmlFor="email">Email address</label>
           <input
@@ -10,7 +26,11 @@ const Contact = () => {
             className="form-control"
             name="email"
             placeholder="email@example.com"
+            {...formik.getFieldProps('email')}
           />
+          {formik.errors.email && formik.touched.email ?
+          
+        :null}
         </div>
         <div className="form-group mt-2">
           <label htmlFor="firstname">First name</label>
