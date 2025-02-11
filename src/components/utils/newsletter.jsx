d8caccd94db804dad2cf0f5e96b804a8a30ac0f5
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-11 15:43:10 +0200

    up

--- src/components/utils/newsletter.jsx
+++ src/components/utils/newsletter.jsx
@@ -1,9 +1,34 @@
+import { useRef } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
 
 const Newsletter = () => {
+  const textInput = useRef();
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = textInput.current.value;
+  };
+
   return (
     <div className="newsletter_container">
       <h1>Join our newsletter</h1>
+      <div className="form">
+        <Form className="mt-4" onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              placeholder="your@email.com"
+              name="emails"
+              ref={textInput}
+            />
+          </Form.Group>
+          <Button className="mt-2" variant="primary" type="submit">
+            Add me to the list
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 };
