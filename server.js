669395e995367c64cb567066d1570ca0b42592ee
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-14 10:27:55 +0200

    up

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
 
+// Адресът към MongoDB и името на базата данни
+const mongoURI = 'mongodb://localhost:27017';
+const dbName = 'име_на_твоята_база_данни';
+
+// Създаване на клиент за MongoDB
+const client = new MongoClient(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const port = process.env.PORT || 3001
 app.listen(port);
