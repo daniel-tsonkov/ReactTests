725ac87d2acc69256fc21ba6824d9c43e97c2372
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-15 21:53:35 +0200

    up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,11 +12,20 @@ const App = () => {
   //       });
   //   };
 
+  const onCarSubmit = () => {
+    axios.post('/api/addcar', {
+      brand: String,
+      model: String,
+      year: Number,
+      avail: Boolean,
+    });
+  };
+
   return (
     <>
       <div className="App">
         <h1>Add car </h1>
-        <button onClick={() => onCarSubmit()}>Add user</button>
+        <button onClick={() => onCarSubmit()}>Add car</button>
       </div>
     </>
   );
