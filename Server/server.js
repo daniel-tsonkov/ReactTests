692fdfaea415eb61e22f2c9290acee35b92494f3
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-17 08:55:45 +0200

    17022025

---
+++ Server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParcer = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+
+// Адресът към MongoDB и името на базата данни
+const mongoURI = 'mongodb://localhost:27017/cars';
+
+mongoose.connect(mongoURI);
+
+app.use(bodyParcer.json());
+
+
+
+const port = process.env.PORT || 3001
+app.listen(port);
