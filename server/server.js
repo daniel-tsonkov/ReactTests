be9c114da4bc752a5795bb82cfc5e83906027c7d
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-20 09:46:49 +0200

    up

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 //PARSING
 app.use(bodyParser.json());
 
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
-    console.log(`RUN ON PORT ${port}`);
+    console.log(`SERVER RUNING ON PORT ${port}`);
 })
