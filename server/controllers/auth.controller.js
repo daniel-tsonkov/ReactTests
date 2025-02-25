1f46a8853ae4b0ab35604f5b580bb532e522e19b
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-25 09:18:11 +0200

    up

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,36 +1,36 @@
+const { authService } = require('../services');
 const httpStatus = require('http-status');
-const { authService } = require('../services')
 
 const authController = {
-    async register(req, res, next) {
+    async register(req, res) {
         try {
             const { email, password } = req.body;
             const user = await authService.createUser(email, password);
             const token = await authService.genAuthToken(user);
 
-            //send verification email
-
+            /// SEND VERIFICATION EMAIL
 
             res.cookie('x-access-token', token)
                 .status(httpStatus.CREATED).send({
                     user,
                     token
-                });
-        } catch (err) {
-            res.status(httpStatus.BAD_REQUEST).send(err.message);
+                })
+        } catch (error) {
+            res.status(httpStatus.BAD_REQUEST).send(error.message);
         }
     },
-    async signin(req, res, next) {
+    async signin(req, res) {
         try {
             const { email, password } = req.body;
             const user = await authService.signInWithEmailAndPassword(email, password);
             const token = await authService.genAuthToken(user);
 
-            res.cookie('x-access-token', token).send({ user, token });
-        } catch (err) {
-            res.status(httpStatus.BAD_REQUEST).send(err.message);
+            res.cookie('x-access-token', token).send({ user, token })
+        } catch (error) {
+            res.status(httpStatus.BAD_REQUEST).send(error.message);
         }
     }
 }
+
 
 module.exports = authController;
--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -2,7 +2,9 @@ const express = require('express');
 const authController = require('../controllers/auth.controller');
 const router = express.Router();
 
+
 router.post('/register', authController.register);
-router.post('/signin', authController.signin)
+router.post('/signin', authController.signin);
+
 
 module.exports = router;
