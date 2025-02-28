c2061fbb3f9525f457c6a184b396c776e6b06134
daniel-tsonkov <daniel_tzonkov@abv.bg>
2025-02-28 09:54:46 +0200

    up

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -5,7 +5,13 @@ const { userService } = require('../services');
 const userController = {
     async profile(req, res, next) {
         try {
-            const user = await userService.findUserById();
+            const user = await userService.findUserById(req.user._id);
+
+            if (!user) {
+                throw new ApiError(httpStatus.NOT_FOUND, 'Use is not found!!!');
+            };
+
+            res.json(user);
         } catch (err) {
             console.log(err);
             next(err);
